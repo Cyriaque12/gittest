33363c59983fdbd090b64dcf73917e8d6baf94d7
Cyriaque12 <42473973+Cyriaque12@users.noreply.github.com>
2019-07-31 17:53:19 +0200

    Create tristana.hpp

---
+++ tristana.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include "OrbWalker.hpp"
+
+
+void testTristana() {
+    cout << "test tristana" << endl;
+}
+
+class Tristana {
+
+public:
+    int health;
+    OrbWalker* orb;
+
+
+    Tristana() {
+        health = 100;
+    }
+
+    void onPostAttack() {
+        cout << "Tristana post attack" << endl;
+    }
+
+    void onUpdate() {
+        cout << "Tristana update" << endl;
+        //orb->fonctions.push_back(onPostAttack);
+        //orb->fonctions.push_back(*onPostAttack);
+        //orb->fonctions.push_back(&onPostAttack);
+        //orb->fonctions.push_back(this->onPostAttack);
+        //orb->fonctions.push_back(*this->onPostAttack);
+        //orb->fonctions.push_back(&this->onPostAttack);
+        orb->fonctions.push_back(*testTristana);
+    }
+
+
+
+
+
+};

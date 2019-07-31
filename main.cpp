f0ef5354c6c00230ad069b54c14a3cf28f259bfd
Cyriaque12 <42473973+Cyriaque12@users.noreply.github.com>
2019-07-31 17:52:59 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "tristana.hpp"
+#include "OrbWalker.hpp"
+
+
+using namespace std;
+
+
+void fonctionTest() {
+    cout << "test" << endl;
+
+}
+
+
+int main()
+{
+    cout << "Hello world!" << endl;
+
+    OrbWalker orb;
+    orb = OrbWalker();
+
+    Tristana* trist;
+    trist = new Tristana();
+
+    trist->orb = &orb;
+    
+
+    orb.fonctions.push_back(fonctionTest);
+    trist->onUpdate();
+
+    orb.onUpdate();
+
+
+    return 0;
+}

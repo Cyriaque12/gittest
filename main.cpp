978927a6567139ef80b6fde256c0c58717e3f995
Cyriaque12 <42473973+Cyriaque12@users.noreply.github.com>
2019-08-08 10:12:45 +0200

    Update main.cpp
    final

--- main.cpp
+++ main.cpp
@@ -5,31 +5,16 @@
 
 using namespace std;
 
-
-void fonctionTest() {
-    cout << "test" << endl;
-
-}
-
-
 int main()
 {
-    cout << "Hello world!" << endl;
-
     OrbWalker orb;
     orb = OrbWalker();
 
     Tristana* trist;
-    trist = new Tristana();
+    trist = new Tristana(&orb);
 
-    trist->orb = &orb;
-    //orb->fonctions.push_back(trist->onPostAttack);
-
-    orb.fonctions.push_back(fonctionTest);
     trist->onUpdate();
-
     orb.onUpdate();
-
 
     return 0;
 }

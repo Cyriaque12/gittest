d9109e54b689532c0a6a28d6e6e11f9ad19857a5
Cyriaque12 <42473973+Cyriaque12@users.noreply.github.com>
2019-08-08 10:13:06 +0200

    Update orbwalker.hpp

--- orbwalker.hpp
+++ orbwalker.hpp
@@ -7,15 +7,14 @@ class OrbWalker {
 
 public:
     vector<void (*)()> fonctions;
-    int test;
+
     OrbWalker() {
-        test = 5;
     }
 
     void onUpdate() {
+        cout << "Orb updating" << endl;
         int len = (int)fonctions.size();
         for (int i = 0; i<len; i++) {
-            cout << "compteur :" << i << endl;
             (this->fonctions[i])();
         }
     }
